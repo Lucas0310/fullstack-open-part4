a4be0729e3f878a956b58423bba25680587f5724
Lucas <lucas.rdl@outlook.com>
2021-05-29 20:45:28 -0300

    refactor: added app file

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
+const app = require('./app')
 const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-
+http.createServer(app)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
